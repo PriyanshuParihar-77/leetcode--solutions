ac190eec7d5ac0db577a3a22f742d66645b1097b
Priyanshu Parihar <84153874+PriyanshuParihar-77@users.noreply.github.com>
2024-09-22 09:48:11 +0530

    Time: 40 ms (100%), Space: 85.2 MB (100%) - LeetHub

--- 3541-report-spam-message/3541-report-spam-message.java
+++ 3541-report-spam-message/3541-report-spam-message.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean reportSpam(String[] message, String[] bannedWords) {
-        Set<String> set = new HashSet<>(Arrays.asList(bannedWords));
+        Set<String> bannedSet = new HashSet<>(Arrays.asList(bannedWords));
         int count = 0;
         for (String word : message) {
-            if (set.contains(word)) {
+            if (bannedSet.contains(word)) {
                 count++;
                 if (count == 2) return true;
             }
